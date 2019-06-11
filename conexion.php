b1dff2d14e3cc5367caf72206855000ba4550f35
Yisusyisus <38733263+Yisusyisus@users.noreply.github.com>
2019-06-11 16:37:30 -0500

    Update conexion.php

--- conexion.php
+++ conexion.php
@@ -5,7 +5,7 @@
 
 		public static function conectar(){
 			$pdo_options[PDO::ATTR_ERRMODE]=PDO::ERRMODE_EXCEPTION;
-			self::$conexion=new PDO('mysql:host=aplicacionweb1010.azurewebsites.net;dbname=mysqldatabase43886','mysqldbuser','Huevos77',$pdo_options);
+			self::$conexion=new PDO('mysql:host=aplicacionweb1010.azurewebsites.net';'dbname=mysqldatabase43886','mysqldbuser','Huevos77',$pdo_options);
 			return self::$conexion;
 		}
 	}
