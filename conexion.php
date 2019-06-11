242b2903a026402cae22d86d665ac276b6aab0c2
Yisusyisus <38733263+Yisusyisus@users.noreply.github.com>
2019-06-11 17:02:54 -0500

    Update conexion.php

--- conexion.php
+++ conexion.php
@@ -1,14 +1,12 @@
-<?php
+<?php 
+	class Db{
+		private static $conexion=null;
+		private function __construct(){}
 
-session_start();
-
-$conn = mysqli_connect(
-    'aplicacionweb1010-mysqldbserver.mysql.database.azure.com',
-    'mysqldbuser@aplicacionweb1010-mysqldbserver',
-    'Huevos77',
-    'mysqldatabase43886'
-);
-    //if(isset($conn)){
-    //echo 'La base de datos esta conectada';
-
+		public static function conectar(){
+			$pdo_options[PDO::ATTR_ERRMODE]=PDO::ERRMODE_EXCEPTION;
+			self::$conexion=new PDO('mysql:host=aplicacionweb1010-mysqldbserver.mysql.database.azure.com;dbname=mysqldatabase43886','mysqldbuser@aplicacionweb1010-mysqldbserver','Huevos77',$pdo_options);
+			return self::$conexion;
+		}
+	}
 ?>
